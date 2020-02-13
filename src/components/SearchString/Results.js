2e9ecfbf0c751f751941e19165b524e8f2c5ea93
Greg Opperman <greg@patientdiscovery.com>
2020-02-12 19:05:41 -0500

    Copy to clipboard

--- src/components/SearchString/Results.js
+++ src/components/SearchString/Results.js
@@ -1,14 +1,28 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import copy from 'copy-to-clipboard'
 
 const SearchStringResults = ({ selectedPKMN }) => {
   const defaultString = "Choose a POKéMON to add it to your search string."
   const searchString = selectedPKMN.length ? selectedPKMN.join() : defaultString
+
+  const showCopyNotification = (e) => {
+    const textbox = e.target
+
+    copy(textbox.innerHTML)
+
+    textbox.value = '* Copied to Clipboard! *'
+    setTimeout(() => {
+      textbox.value = searchString
+    }, 1200)
+  }
+
   return (
     <div className="searchstring__container">
       <textarea
         className="searchstring__results"
         value={searchString}
+        onClick={showCopyNotification}
         readOnly>
       </textarea>
     </div>
